f561467d951b8756fe5124db3bf7850f25b9d895
Ewan Bigotte <ewan.bigotte@epitech.eu>
2022-11-03 12:09:17 +0100

    fix

---
+++ lib/tmp/myAudio.h
@@ -0,0 +1,16 @@
+
+#ifndef MYAUDIO_H
+    #define MYAUDIO_H
+
+    #include "./Audio/Export.h"
+    #include "./Audio/Listener.h"
+    #include "./Audio/Music.h"
+    #include "./Audio/Sound.h"
+    #include "./Audio/SoundBuffer.h"
+    #include "./Audio/SoundBufferRecorder.h"
+    #include "./Audio/SoundRecorder.h"
+    #include "./Audio/SoundStatus.h"
+    #include "./Audio/SoundStream.h"
+    #include "./Audio/Types.h"
+
+#endif // MYAUDIO_H
